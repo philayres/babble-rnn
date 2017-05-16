026e72032e2de58c253cfb320160420e47691acd
root <root@localhost>
2017-05-16 12:25:10 +1000

    try gru

--- lstm_c2_generation.py
+++ lstm_c2_generation.py
@@ -6,7 +6,7 @@
 from __future__ import print_function
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-from keras.layers import LSTM
+from keras.layers import GRU
 from keras.optimizers import RMSprop
 from keras.utils.data_utils import get_file
 import numpy as np
@@ -84,7 +84,7 @@ for i, sentence in enumerate(sentences):
 # build the model: a single LSTM
 print('Build model...')
 model = Sequential()
-model.add(LSTM(128, input_shape=(maxlen, framelen)))
+model.add(GRU(128, input_shape=(maxlen, framelen)))
 model.add(Dense(framelen))
 model.add(Dense(framelen))
 model.add(Dense(framelen))
