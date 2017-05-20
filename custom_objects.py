a3810372e4d3a0f124b51e5c7a52122d877492e3
Phil Ayres <phil.ayres@consected.com>
2017-05-20 02:25:58 +0000

    refactor add custom objects to enable model load logging csv

---
+++ custom_objects.py
@@ -0,0 +1,34 @@
+from keras import backend as K
+import numpy as np
+
+#frame_property_bits 
+#1,1,1,1, # voiced flags for 4 PCM frames
+#7, #Wo
+#5, #E
+#16,16,16,16,16,16,16,8,8,4 #LSP
+
+frame_prop_loss_scale = np.array([
+   16,16,16,16,
+   1,
+   4,
+   8,8,8,8,8,8,8,16,16,32
+  ])
+
+
+class CustomObjects:
+
+
+  def __init__(self, utils):
+    utils.log("frame_prop_loss_scale: ", str(frame_prop_loss_scale))
+
+  # Define a custom loss calculation, allowing the individual properties
+  # of the Codec 2 frame to be represented, based on their relative size
+
+  @staticmethod
+  def codec2_param_error(y_true, y_pred):
+    
+    np.multiply(y_pred, frame_prop_loss_scale)
+    np.multiply(y_true, frame_prop_loss_scale)
+    
+    # perform a basic mean absolute error calculation
+    return K.mean(K.abs(y_pred - y_true), axis=-1)
--- lstm_c2_generation.py
+++ lstm_c2_generation.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
 
 from model_utils import ModelUtils
+from custom_objects import CustomObjects
 
 # check command line args before loading everything, to save time
 
 utils = ModelUtils()
-
+custom_objects = CustomObjects(utils)
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
@@ -13,20 +14,25 @@ from keras.layers import LSTM
 from keras.optimizers import Nadam # SGD #Adam #RMSprop
 from keras.utils.data_utils import get_file
 from keras import backend as K
+
+
 import numpy as np
 import random
 import time
 import sys
 import os
+import signal
+  
+signal.signal(signal.SIGINT, utils.signal_handler)
 
 # number of training iterations
 num_iterations = 600
 
 fit_batch_size = 128
-print("fit_batch_size: ", fit_batch_size)
+utils.log("fit_batch_size: ", fit_batch_size)
 # length of frame sequence to generate
 genlen=400
-print("genlen: ", genlen)
+utils.log("genlen: ", genlen)
 # generate sample data every nth iteration
 gen_every_nth = 10
 
@@ -35,34 +41,19 @@ gen_every_nth = 10
 framelen=16
 
 # length of frame sequence for learning
-frame_seq_len = 40 # 200 # 5 seconds of audio
-print("frame_seq_len: ", frame_seq_len)
+frame_seq_len = 4 # 200 # 5 seconds of audio
+utils.log("frame_seq_len: ", frame_seq_len)
 step = 3
 
-#frame_property_bits 
-#1,1,1,1, # voiced flags for 4 PCM frames
-#7, #Wo
-#5, #E
-#16,16,16,16,16,16,16,8,8,4 #LSP
 
-
-
-frame_prop_loss_scale = np.array([
- 64,64,64,64,
- 1,
- 4,
- 8,8,8,8,8,8,8,16,16,32
-])
-print("frame_prop_loss_scale: ", str(frame_prop_loss_scale))
-
-print("loading test data from: ", utils.testdata_filename)
+utils.log("loading test data from: ", utils.testdata_filename)
 testdata = np.fromfile(utils.testdata_filename, dtype=np.uint8)
 
 len_testdata = len(testdata)
 
 num_frames = int(len_testdata / framelen)
 
-print('corpus length:', len_testdata)
+utils.log('corpus length:', len_testdata)
 
 
 
@@ -78,7 +69,7 @@ for j in range(0, num_frames):
     i = j * framelen
     all_frames.append(testdata[i: i + framelen])
 
-print('number of frames:', len(all_frames))
+utils.log('number of frames:', len(all_frames))
 
 # pull the frames into frame sequences (frame_seqs) of frame_seq_len frames
 # pull a single frame following each frame sequence into an array of next_frames
@@ -86,7 +77,7 @@ for i in range(0, num_frames - frame_seq_len, step):
     frame_seqs.append(all_frames[i: i + frame_seq_len])
     next_frames.append(all_frames[i + frame_seq_len])
 
-print('number of frame sequences:', len(frame_seqs))
+utils.log('number of frame sequences:', len(frame_seqs))
 
 
 print('initialising input and expected output arrays')
@@ -103,41 +94,33 @@ for i, frame_seq in enumerate(frame_seqs):
         #X[i,t] = frame
 
 
-# Define a custom loss calculation, allowing the individual properties
-# of the Codec 2 frame to be represented, based on their relative size
-def codec2_param_error(y_true, y_pred):
-  
-  np.multiply(y_pred, frame_prop_loss_scale)
-  np.multiply(y_true, frame_prop_loss_scale)
-  
-  # perform a basic mean absolute error calculation
-  return K.mean(K.abs(y_pred - y_true), axis=-1)
-
 # Define a new model
 # only used if a model is not loaded from a file
 def define_model():
     model =  Sequential()
     model.add(LSTM(
-        20 
+        80 
         ,input_shape=(frame_seq_len, framelen) 
-#        ,return_sequences=True
+        ,return_sequences=True
       )
     ) 
-#    model.add(LSTM(
-#        40
-#        , return_sequences=True
-#        )
-#      )
-#    model.add(LSTM(40))
+    model.add(LSTM(
+        80
+        , return_sequences=True
+      )
+    )
+    model.add(LSTM(80))
 
-#    model.add(Dense(framelen))
+    model.add(Dense(framelen))
+    model.add(Dense(framelen))
+    model.add(Dense(framelen))
     model.add(Dense(framelen))
 
     #model.add(Dropout(0.02))
     #model.add(Activation('relu'))
     
     optimizer = Nadam() #SGD() #Adam() #RMSprop(lr=0.01)
-    loss = codec2_param_error #'mean_absolute_error'
+    loss = CustomObjects.codec2_param_error #'mean_absolute_error'
     
     model.compile(loss=loss, optimizer=optimizer)
     return model
@@ -163,10 +146,10 @@ def gen_sequence(iteration):
 model = None
 
 if len(utils.model_filename) > 0:
-
-  utils.load_model()    
+  model = utils.load_model()   
+  utils.save_json_model(model)
 else:
-  print("creating new model")
+  utils.log("creating new model")
   model = define_model()
   utils.save_json_model(model)
 
@@ -174,13 +157,15 @@ else:
 # output generated frames after nth iteration
 for iteration in range(1, num_iterations + 1):
   print('-' * 50)
-  print('Iteration', iteration)
+  utils.log('Iteration', iteration)
+  
 
-  model.fit(X, y, batch_size=fit_batch_size, nb_epoch=1)
+  model.fit(X, y, batch_size=fit_batch_size, nb_epoch=1,
+   callbacks=[utils.csv_logger])
 
   if gen_sequence(iteration):
     ofn = utils.open_output_file(iteration)
-    print("saving generated sample output to: ", ofn)
+    utils.log("saving generated sample output to: ", ofn)
   else:
     print("not generating samples this iteration")  
 
@@ -190,7 +175,7 @@ for iteration in range(1, num_iterations + 1):
     start_index = random.randint(0, num_frames - frame_seq_len - 1)
     start_time = 1.0 * start_index / 40
     
-    print("seed sequence for generation starts at frame index: ", start_index, " (approx. ", int(start_time / 60), ":", int(start_time % 60), ")" )
+    utils.log("seed sequence for generation starts at frame index: ", start_index, " (approx. ", int(start_time / 60), ":", int(start_time % 60), ")" )
 
     # pick the seed frame sequence starting at the random start index, with frame_seq_len frames
     seed_frame_seq = all_frames[start_index: start_index + frame_seq_len]
@@ -223,11 +208,12 @@ for iteration in range(1, num_iterations + 1):
       seed_frame_seq.append(next_frame)
 
     # write the seed + generated data to the output file
-    print ("writing output file to disk")
+    print("writing output file to disk")
     for frame in generated:
       utils.output_file.write(frame)
         
     utils.output_file.close()
+    utils.log("wrote frames: ", len(generated))
     
     if gen_sequence(iteration):
       print("saving .h5 model file")
