bea747aed15e8db519871bf6f7af23a3e625b32d
Phil Ayres <ayres.phil@gmail.com>
2017-07-16 22:58:45 +0100

    quick fix for 3200 rate codec

--- c2towav.sh
+++ c2towav.sh
@@ -1,4 +1,6 @@
 fn=$1
 
-~/store/codec2/Vocoder1300/test/c2dec $fn  $fn.raw charbits
-sox -r 8000 -b 16 -c 1 -e signed  $fn.raw $fn.wav                                                    
+#~/store/codec2/Vocoder1300/test/c2dec $fn  $fn.raw charbits
+#sox -r 8000 -b 16 -c 1 -e signed  $fn.raw $fn.wav                                                    
+~/personal_projects/codec2-babble/build_linux/src/c2dec 3200 $fn $fn-3200.raw --plainbytes
+sox -r 8000 -b 16 -c 1 -e signed  $fn-3200.raw $fn-3200.wav
