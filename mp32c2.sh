1652ead6d8f101d24731a27eb40e86d50590e0c1
Phil Ayres <ayres.phil@gmail.com>
2017-07-18 18:32:13 +0100

    generate codec2 from mp3

--- mp32c2.sh
+++ mp32c2.sh
@@ -5,11 +5,14 @@ lame $fn.mp3 -m m --decode $fn.wav
 sox $fn.wav -c 1 -r 8000 -b 16 -e signed $fn-8k.wav
 sox $fn-8k.wav -c 1 -r 8000 -b 16 -e signed $fn-8k.raw
 
-~/store/codec2/Vocoder1300/test/c2enc $fn-8k.raw $fn.c2cb charbits  
+path_to_c2=~/personal_projects/codec2-babble/build_linux/src
+#~/store/codec2/Vocoder1300/test/c2enc $fn-8k.raw $fn.c2cb charbits  
+LD_LIBRARY_PATH=$path_to_c2 $path_to_c2/c2enc 3200 $fn-8k.raw $fn.c2cb-3200 --plainbytes
 
-~/store/codec2/Vocoder1300/test/c2dec $fn.c2cb $fn.c2cb.raw charbits  
+#~/store/codec2/Vocoder1300/test/c2dec $fn.c2cb $fn.c2cb.raw charbits  
+LD_LIBRARY_PATH=$path_to_c2 $path_to_c2/c2dec 3200 $fn.c2cb-3200 $fn.c2cb-3200.raw --plainbytes
 
-sox  -c 1 -r 8000 -b 16 -e signed $fn.c2cb.raw $fn.c2cb.wav
+sox  -c 1 -r 8000 -b 16 -e signed $fn.c2cb-3200.raw $fn.c2cb-3200.wav
 
 cp $fn.c2cb ~/store/c2gen/test
 
