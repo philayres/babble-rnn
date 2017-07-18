9c86fb582420457d9eefe114d75218ade9cf4328
Phil Ayres <ayres.phil@gmail.com>
2017-07-18 09:34:03 +0100

    fix 3200 rate operation

--- c2towav.sh
+++ c2towav.sh
@@ -1,6 +1,8 @@
 fn=$1
+path_to_c2=~/personal_projects/codec2-babble/build_linux/src
+#path_to_c2=~/store/codec2/Vocoder1300/test
 
-#~/store/codec2/Vocoder1300/test/c2dec $fn  $fn.raw charbits
+#$path_to_c2/c2dec $fn  $fn.raw charbits
 #sox -r 8000 -b 16 -c 1 -e signed  $fn.raw $fn.wav                                                    
-~/personal_projects/codec2-babble/build_linux/src/c2dec 3200 $fn $fn-3200.raw --plainbytes
+LD_LIBRARY_PATH=$path_to_c2 $path_to_c2/c2dec 3200 $fn $fn-3200.raw --plainbytes
 sox -r 8000 -b 16 -c 1 -e signed  $fn-3200.raw $fn-3200.wav
