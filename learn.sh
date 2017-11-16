dfb67a41584bfb596a2fba3633f1593085774bbc
philayres <phil.ayres@consected.com>
2017-11-16 09:38:44 +0000

    Learning func-20-3-1 ./test/criticalpracticalreason.c2-3200

--- learn.sh
+++ learn.sh
@@ -13,6 +13,16 @@ nohup python3 lstm_c2_generation.py $1 $2 $3 > nohup/$1.out &
 # Wait for up to 10 seconds then commit the new config and model def
 wait_file "./out/$1/config.json" && {
   touch out/$1/training.log
+  NOTES_FILE=out/$1/notes.md
+  echo "# $1 Notes" > $NOTES_FILE
+  echo "" >> $NOTES_FILE
+  dt=$(date '+%F %H:%M:%S'); echo "$dt" >> $NOTES_FILE
+  echo "" >> $NOTES_FILE
+  echo "Run with arguments $2 $3" >> $NOTES_FILE
+  echo "" >> $NOTES_FILE
+  echo "## Description" >> $NOTES_FILE
+  echo "" >> $NOTES_FILE
+  editor $NOTES_FILE
   git add out/$1/config.json
   git add out/$1/training.log
   git commit -a -m "Learning $1 $2 $3"
