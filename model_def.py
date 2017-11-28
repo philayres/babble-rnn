f4b0841461d9a6d85f5d0ba4fead80377ff883e8
philayres <phil.ayres@consected.com>
2017-11-28 19:45:11 +0000

    Learning func-21-5-1 ./test/critiquepracticalreason_00_kant_64kb.c2cb-3200 none

--- model_def.py
+++ model_def.py
@@ -135,11 +135,10 @@ class ModelDef(object):
 
     mp = MaxPooling2D(in_scale, padding='same', data_format='channels_last')
     print(mp.get_config())
-    print(mp.output_shape)
-    print(mp.output_shape)
     mp0 = mp(conv0)
-
     rs1 = TimeDistributed(keras.layers.Reshape((framelen,)))(mp0)
+    print(mp0.input_shape)
+    print(mp0.output_shape)
 
     # Need to repeat here
     rp0 = RepeatVector(in_scale)(rs1)
