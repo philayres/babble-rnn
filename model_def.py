eae0e90f2dafd242ba73337c39d55128e0b37e36
philayres <phil.ayres@consected.com>
2017-12-06 10:35:18 +0000

    Learning func-23-2-5 ./test/criticalpracticalreason.c2-3200 none

--- model_def.py
+++ model_def.py
@@ -174,7 +174,7 @@ class ModelDef(object):
               conv_count,
               kernel_size=(3,13),
               padding='valid',
-              strides=(4,14),
+              strides=(2,14),
               activation='relu',
               data_format="channels_last",
               trainable=self.decoder_trainable
