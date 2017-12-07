ed1bd4dcb73f4effe39cd79e1d59de14251ac97d
philayres <phil.ayres@consected.com>
2017-12-07 10:02:37 +0000

    Learning func-24-1-1 ./test/critiquepracticalreason_00_kant_64kb.c2cb-3200

--- model_def.py
+++ model_def.py
@@ -40,7 +40,7 @@ class ModelDef(object):
     overlap_sequence = config.overlap_sequence
     short_input_len = frame_seq_len - overlap_sequence*2
 
-    self.conv_count = 65
+    self.conv_count = 48
     enc_params = 20
 
 
