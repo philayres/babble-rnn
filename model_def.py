88aa6323105845193a24c37fc6aa96c933964bc1
philayres <phil.ayres@consected.com>
2017-12-12 15:40:36 +0000

    Learning func-26-1-9 ./test/critiquepracticalreason_00_kant_64kb.c2cb-3200 --load-weights=out/func-26-1-9/weights-2560.h5

--- model_def.py
+++ model_def.py
@@ -97,6 +97,13 @@ class ModelDef(object):
         , trainable=generator_trainable
     )(res)
 
+    res = LSTM(
+        128
+        , return_sequences=True
+        , name='generator_LSTM_3'
+        , trainable=generator_trainable
+    )(res)
+
     conf = TimeDistributed(
         Dense(
           enc_params
