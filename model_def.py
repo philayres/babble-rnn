4d7347dc81d7439cac65c45366c5ef78f6869112
philayres <phil.ayres@consected.com>
2018-01-11 20:04:32 +0000

    Learning func-27-1-20 ./test/critiquepracticalreason_00_kant_64kb.c2cb-3200 --load-weights=out/func-27-1-20/weights-13700.h5

--- model_def.py
+++ model_def.py
@@ -18,8 +18,8 @@ class ModelDef(object):
 
   stateful = False
 
-  encoder_trainable = False
-  decoder_trainable = False
+  encoder_trainable = True
+  decoder_trainable = True
   generator_trainable = True
 
 
@@ -517,8 +517,8 @@ class ModelDef(object):
     encoder_loss_prop = 0
 
     main_loss_prop = 0.0
-    mid_loss_prop = 0.1
-    generator_loss_prop = 0.9
+    mid_loss_prop = 0.01
+    generator_loss_prop = 0.99
 
     if self.decoder_trainable and not self.generator_trainable:
       main_loss_prop = 0
