a61d15407ade52f90501f8c6c188f15f62adfe76
philayres <phil.ayres@consected.com>
2018-01-21 00:23:37 +0000

    Learning func-27-1-29 test/critiquepracticalreason_00_kant_64kb.c2cb-3200

--- model_def.py
+++ model_def.py
@@ -18,8 +18,8 @@ class ModelDef(object):
 
   stateful = False
 
-  encoder_trainable = False
-  decoder_trainable = False
+  encoder_trainable = True
+  decoder_trainable = True
   generator_trainable = True
 
 
@@ -84,21 +84,21 @@ class ModelDef(object):
       )
     )
     res = conf(res)
-    # print(conf.get_config())
-    # print(conf.input_shape)
-    # print(conf.output_shape)
-    #
-    # res = TimeDistributed(Dropout(0.05))(res)
-    # res = Bidirectional(
-    #   LSTM(
-    #     256
-    #     , return_sequences=True
-    #     , name='generator_LSTM_1'
-    #     , trainable=generator_trainable
-    #     , kernel_initializer='he_normal'
-    #     , recurrent_initializer='he_normal'
-    #   )
-    # )(res)
+    print(conf.get_config())
+    print(conf.input_shape)
+    print(conf.output_shape)
+
+    res = TimeDistributed(Dropout(0.05))(res)
+    res = Bidirectional(
+      LSTM(
+        256
+        , return_sequences=True
+        , name='generator_LSTM_1'
+        , trainable=generator_trainable
+        , kernel_initializer='he_normal'
+        , recurrent_initializer='he_normal'
+      )
+    )(res)
 
     res = TimeDistributed(Dropout(0.05))(res)
     conf = Bidirectional(
